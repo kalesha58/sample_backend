dcf6fd0be071af427a2a5915f6126f0a4b096ce3
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2022-12-12 08:10:27 +0530

    Ad1d

--- dataBase/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-
-const connection =async()=>{
-    await mongoose.connect(process.env.MONGODB_URL,()=>{
-        console.log("DB")
-    })
-}
-module.exports=connection
